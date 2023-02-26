2fe55d8869f48ade814f9d223885bfb099021b82
Andrey Pechennikov <ap@devandy.me>
2023-02-26 20:52:05 +0400

    Fix header component

--- guitar-shop/apps/frontend/src/components/header/header.tsx
+++ guitar-shop/apps/frontend/src/components/header/header.tsx
@@ -1,44 +1,67 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import { AppRoute } from '../../conts';
 import Logo from '../logo/logo';
 import { useAppSelector } from '../../hooks';
-import { getUser } from '../../store/user-data/selectors';
+import { getIsAdmin, getUser } from '../../store/user-data/selectors';
 
 function Header(): JSX.Element {
+  const [currentLink, setCurrentLink] = useState(AppRoute.Main);
   const user = useAppSelector(getUser);
+  const isAdmin = useAppSelector(getIsAdmin);
+
+  const onClickHandler = (link: AppRoute) => {
+    return () => setCurrentLink(link);
+  }
 
   return (
-    <header className="header" id="header">
+    <header className={`header${user ? ' header--logged' : ''}`} id="header">
       <div className="container">
         <div className="header__wrapper">
           <Logo />
           <nav className="main-nav">
             <ul className="main-nav__list">
               <li className="main-nav__item">
-                <Link className="link main-nav__link link--current" to={AppRoute.Main}>
+                <Link
+                  className={`link main-nav__link${currentLink === AppRoute.Main ? ' link--current' : ''}`}
+                  to={AppRoute.Main}
+                  onClick={onClickHandler(AppRoute.Main)}
+                >
                   Каталог
                 </Link>
               </li>
-              <li className="main-nav__item">
-                <Link className="link main-nav__link" to={AppRoute.Cart}>
-                  Где купить?
-                </Link>
-              </li>
-              <li className="main-nav__item">
-                <Link className="link main-nav__link" to={AppRoute.Main}>
-                  О компании
-                </Link>
-              </li>
+              { isAdmin &&
+                <li className="main-nav__item">
+                  <Link
+                    className={`link main-nav__link${currentLink === AppRoute.Orders ? ' link--current' : ''}`}
+                    to={AppRoute.Orders}
+                    onClick={onClickHandler(AppRoute.Orders)}
+                  >
+                    Список заказов
+                  </Link>
+                </li>
+              }
+              { isAdmin &&
+                <li className="main-nav__item">
+                  <Link
+                    className={`link main-nav__link${currentLink === AppRoute.Products ? ' link--current' : ''}`}
+                    to={AppRoute.Products}
+                    onClick={onClickHandler(AppRoute.Products)}
+                  >
+                    Список товаров
+                  </Link>
+                </li>
+              }
             </ul>
           </nav>
           <div className="header__container">
-            <span className="header__user-name">Имя</span>
+            <span className="header__user-name">{user?.name}</span>
             <Link className="header__link" to={AppRoute.Login} aria-label="Перейти в личный кабинет">
               <svg className="header__link-icon" width="12" height="14" aria-hidden="true">
                 <use xlinkHref="#icon-account"></use>
               </svg>
-              <span className="header__link-text">{ user ? user.name : 'Вход' }</span>
+              <span className="header__link-text">Вход</span>
             </Link>
             <Link className="header__cart-link" to={AppRoute.Cart} aria-label="Перейти в корзину">
               <svg className="header__cart-icon" width="14" height="14" aria-hidden="true">
